c9f38fbedefcf4e1c34d59aaac1a0410bc1eaaf0
rindangtulus <rindangtulus@gmail.com>
2026-02-11 00:30:51 +0700

    fix: repair program.cs

--- Program.cs
+++ Program.cs
@@ -1,40 +1,42 @@
 using Microsoft.EntityFrameworkCore;
 using _2026_ruangkita_backend.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlite("Data Source=ruangkita.db"));
+    options.UseSqlite("Data Source=ruangkita.db"));
 
-builder.Services.AddControllers();
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+});
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.AllowAnyOrigin() // Mengizinkan dari alamat mana saja (cocok untuk tahap belajar)
-                  .AllowAnyMethod() // Mengizinkan GET, POST, PUT, DELETE, PATCH
-                  .AllowAnyHeader(); // Mengizinkan header apa saja
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
         });
 });
+
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseCors("AllowAll");
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
